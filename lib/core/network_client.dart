218044a0d59fe6f98e14466d7b044da2cdaaf018
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-07-08 17:11:02 +0300

    updated network clientr

--- lib/core/network_client.dart
+++ lib/core/network_client.dart
@@ -3,7 +3,8 @@ import 'package:dio/dio.dart';
 class NetworkClient {
 
   // BaseURL of the API
-  final String baseUrl = 'https://jsonplaceholder.typicode.com';
+  //https://api.punkapi.com/v2/beers?page=2&per_page=80
+  final String baseUrl = 'https://api.punkapi.com/v2';
 
   // Dio instance
   final Dio dio = Dio();
